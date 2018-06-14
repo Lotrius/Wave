9e65ae30ea3e7b228e2a61ea1be660a0089a6a39
EunhoKim219 <eunho.kim@stonybrook.edu>
2018-06-14 16:45:48 -0400

    tried music

--- Wave/src/Gameplay/GameSetUp.java
+++ Wave/src/Gameplay/GameSetUp.java
@@ -8,21 +8,27 @@ package Gameplay;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
 
 /**
  *
  * @author Chris
  */
 public class GameSetUp extends BasicGame{
-
+    private Music music;
+    private Sound sound;
     public GameSetUp(String title) {
         super(title);
     }
 
     @Override
     public void init(GameContainer gc) throws SlickException {
-
+        music = new Music("Music/song.ogg");
+        music.setVolume(1.0f);
+        music.loop();
+        //sound = new Sound();
     }
 
     @Override
