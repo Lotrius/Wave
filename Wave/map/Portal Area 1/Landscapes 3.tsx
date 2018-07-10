fa0751375265e6829136aca7df0952ce60e85dc2
Somesk278 <skim7420@gmail.com>
2018-07-10 14:33:53 -0400

    added collision data

--- Wave/map/Portal Area 1/Landscapes 3.tsx
+++ Wave/map/Portal Area 1/Landscapes 3.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Landscapes 3" tilewidth="32" tileheight="32" tilecount="900" columns="30">
  <image source="../../art/art-3.png" width="960" height="960"/>
+ <tile id="802">
+  <objectgroup draworder="index">
+   <object id="1" x="3.63636" y="15.0909">
+    <polyline points="0,0 4,-8.36364 7.09091,-11.0909 17.8182,-11.4545 22,-7.45455 25.4545,0 25.8182,4.54545 22.7273,11.4545 18,15.2727 8.18182,15.2727 2.54545,11.0909 0.727273,6.18182 0,0.363636"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
