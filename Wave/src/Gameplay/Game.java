2f516ee0c7a14969e316315c5c35c8d50fd4f901
Somesk278 <skim7420@gmail.com>
2018-07-12 16:48:19 -0400

    opening screen

--- Wave/src/Gameplay/Game.java
+++ Wave/src/Gameplay/Game.java
@@ -9,19 +9,31 @@ import org.newdawn.slick.state.StateBasedGame;
  *
  * @author Solomon
  */
-public class Game extends StateBasedGame{
-    public static void main(String[] args) throws SlickException {
-        AppGameContainer app = new AppGameContainer((org.newdawn.slick.Game) new GameSetUp("Wave"));
-        app.setDisplayMode(1920, 1080, true);
-        app.start();
-    }
+public class Game extends StateBasedGame {
 
-    public Game(String name) {
-        super(name);
+    public static final String gameName = "Wave";
+    public static final int opening = 0;
+
+    public Game(String gameName) {
+        super(gameName);
+        this.addState(new OpeningScreen(opening));
     }
 
     @Override
     public void initStatesList(GameContainer gc) throws SlickException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.getState(opening).init(gc, this);
+        this.enterState(opening);
     }
+
+    public static void main(String[] args) {
+        AppGameContainer app;
+        try {
+            app = new AppGameContainer(new Game(gameName));
+            app.setDisplayMode(1920, 1080, true);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- Wave/src/Gameplay/OpeningScreen.java
+++ Wave/src/Gameplay/OpeningScreen.java
@@ -19,17 +19,24 @@ import org.newdawn.slick.state.StateBasedGame;
  *
  * @author solomon
  */
-public class OpeningScreen extends BasicGameState{
+public class OpeningScreen extends BasicGameState {
+
     private Music music;
     private Sound sound;
+    private Image img;
+
+    public OpeningScreen(int opening) {
+
+    }
 
     @Override
     public int getID() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 0;
     }
 
     @Override
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        img = new Image("art/wave.jpg");
         music = new Music("Music/tempIntroSong.ogg");
         music.setVolume(1.0f);
         music.loop();
@@ -38,12 +45,12 @@ public class OpeningScreen extends BasicGameState{
 
     @Override
     public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        grphcs.drawImage(img, 0 , 0);
     }
 
     @Override
     public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
-                if(gc.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
+        if (gc.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
             gc.exit();
         }
     }
